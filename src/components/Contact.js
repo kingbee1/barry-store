35188eb6c3d5edcc45fc65667936741064e3233a
Abiodun <abiodunhodonu@gmail.com>
2022-06-15 22:57:29 +0100

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './styles/Contact.scss'
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
   return (
     <div className='contact'>
-      contatti
+      <Link className='a_home' to='/' >Back</Link>
+      <div className="inputs">
+        <h1>Contact Us</h1>
+        <input type="text" placeholder='Name' />
+        <input type="text" placeholder='Phone Contact' />
+        <input type="text" placeholder='Message' />
+        <button>Send</button>
+      </div>
     </div>
   )
 }
